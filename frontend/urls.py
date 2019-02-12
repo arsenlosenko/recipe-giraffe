00d20dd4b5a2f28845a4c5db0f9645440e65ecdc
Arsen Losenko <arsenlosenko@gmail.com>
2019-02-12 18:02:36 +0200

    Add static files and navbar

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from frontend.views import RecipeListView, RecipeDetailView
 
 app_name = 'frontend'
 urlpatterns = [
-    path('r/', RecipeListView.as_view(), name="list-recipes"),
-    path('r/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail')
+    path('', RedirectView.as_view(pattern_name="frontend:list-recipes"), name="home"),
+    path('recipes/', RecipeListView.as_view(), name="list-recipes"),
+    path('recipes/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail')
 ]
